cab2a9063025e8bc877f972f1ca6ecc0594ae957
Gustavo de Oliveira Fagundes <jamesgustavo133@gmail.com>
2024-11-11 18:01:17 -0300

    Adicionando pasta "helpers" com funções utilitárias para facilitar o desenvolvimento
    Este commit cria uma nova pasta chamada `helpers`, onde serão centralizadas funções utilitárias para agilizar e simplificar o trabalho no código. Essas funções de ajuda rápida visam reduzir redundâncias e melhorar a legibilidade e a eficiência do desenvolvimento, concentrando operações comuns e rotinas reutilizáveis em um único local.

---
+++ helpers/helpers.php
@@ -0,0 +1,7 @@
+<?php
+
+//Pegando o dominio que está rodando a aplicação para integrar no sistema de rotass
+function dominio(){
+   return "http://localhost/prova-fullstack-nov-24-one-payments";  
+}
+
